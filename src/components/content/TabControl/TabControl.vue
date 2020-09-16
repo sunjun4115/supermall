bd7f69b38e0a1603baa11220c0f75622c00be14b
unknown <jun.sun@qualvision.cn>
2020-09-16 19:10:41 +0800

    add better-scroll

---
+++ src/components/content/TabControl/TabControl.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="tabControl">
+        <div v-for="(item, index) in titles" :key="index" class="tabCotrol-item" 
+        :class="{activeC:index === currentIndex}"
+        @click='itemClick(index)'>
+            <span>{{item}}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'TabControl',
+    props:{
+        titles:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+        
+    },
+    data(){
+        return {
+            currentIndex:0
+        }
+    },
+    methods:{
+        itemClick(index){
+            return this.currentIndex = index
+        }
+    }
+}
+</script>
+<style scoped>
+.tabControl{
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background: #ffffff;
+}
+.tabCotrol-item{
+    flex: 1;
+   
+}
+.tabCotrol-item span{
+ padding: 5px;
+}
+.activeC{
+    color: var(--color-high-text);
+    
+}
+.activeC span{
+border-bottom:2px solid var(--color-tint)
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,16 +1,123 @@
 <template>
     <!-- <h2>主页</h2> -->
-    <div>
+    <div id="home">
         <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
         <home-swiper :banner="banner"></home-swiper>
         <recommend-view :recommend = 'recommend'></recommend-view>
+        <feature-view></feature-view>
+        <tab-control class="tabControlCX" :titles="['流行','新款','精选']"></tab-control>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </div>
   
 </template>
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/TabControl/TabControl'
+
 import HomeSwiper from './childComps/homeSwiper'
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
 import {getHomeMultidata} from 'network/home'
 
@@ -26,8 +133,10 @@ export default {
     },
     components:{
         NavBar,
+        TabControl,
         HomeSwiper,
-        RecommendView
+        RecommendView,
+        FeatureView
     },
     created(){
         //1.请求多个数据
@@ -41,8 +150,20 @@ export default {
 }
 </script>
 <style scoped>
+#home{
+    padding-top: 44px;
+}
 .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+}
+.tabControlCX{
+    position: sticky;
+    top:44px
 }
 </style>
